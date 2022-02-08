286b0565e157beb1c3f7103f659a51bc3bfecb12
louis marzorati <louismarzorati@gmail.com>
2022-02-08 12:14:38 -0500

    navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <h1>Scratchin the itch</h1>
+      <ul className="links">
+        <li>
+          <Link href="/swipe">
+            <a>swipe</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/animate/blocks">
+            <a>blocks</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/animate/snazzy">
+            <a>snzy</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
